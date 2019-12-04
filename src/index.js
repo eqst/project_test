d840acfab1555bc9f3d3aa7403cdc1740f011ead
eqst <1344127397@qq.com>
2019-12-04 20:19:42 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
+import Logo from './assets/imgs/bglogin1.jpg'
+import "./assets/css/mycss.css"
+
+const image = new Image()
+image.src = Logo
+document.body.appendChild(image)
 console.log('Hello Webpack!')
-document.getElementById('root').innerHTML = '<h1>Hello222</h1>'
+document.getElementById('root')
+.innerHTML = '<h1>Hello3332</h1>'
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 //引入path插件
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 module.exports = {
   mode:'production',
@@ -12,18 +13,53 @@ module.exports = {
     path:path.resolve(__dirname,'dist')
   },
   module:{
+    
     rules:[
+      {
+        test: /\.js$/,
+        //exclude: /(node_modules|bower_components)/,
+        include: path.resolve(__dirname, 'src'),
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'], // 多个loader从右到左处理
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 1000,
+          name: 'static/img/[name].[hash:7].[ext]' // 相对于output.path
+        }
+      },
+      {
+        test: /\.vue$/,
+        include: path.resolve(__dirname, 'src'),
+        loader: 'vue-loader'
+      },
 
+      {
+        test: /\.css$/,
+        use: ['vue-style-loader', 'css-loader'],
+      },
+      new VueLoaderPlugin()
     ]
   },
   plugins:[
     new HtmlWebpackPlugin({
-      template:'index.html',
-      firename:'index.html'
+      template: 'index.html',
+      filename: 'index.html'
     })
   ],
   devServer:{
     open:true,
     quiet:true
-  }
+  },
+  devtool: 'cheap-module-eval-source-map',
 }
